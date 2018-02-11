708f764ad80301d0af72687fde741687768845d6
Xavier Dumesnil <xavier@frontapp.com>
2018-02-10 17:47:40 -0800

    Add homebrew script

---
+++ extra/homebrew.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Make sure we’re using the latest Homebrew
+brew update
+
+###############################################################################
+# CLI
+
+brew install node
+brew install git
+
+## Dotfiles management
+brew tap thoughtbot/formulae
+brew install thoughtbot/formulae/rcm
+
+###############################################################################
+# Softwares
+
+brew cask install front
+
+brew cask install 1password
+brew cask install authy
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install kap
+brew cask install pritunl
+brew cask install sequel-pro
+brew cask install slack
